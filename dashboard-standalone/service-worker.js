3d2e04d7b2e7acf683f9191eccf541dd0f7c0bfb
Mehedi Hasan <158504182+mehedi-exx@users.noreply.github.com>
2025-07-14 23:31:08 +0600

    Add files via upload

---
+++ dashboard-standalone/service-worker.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'g9tool-dashboard-v1';
+const urlsToCache = [
+  './dashboard.html',
+  './dashboard-styles.css',
+  './js/utils.js',
+  './js/productGenerator.js',
+  './languages/en.json',
+  './languages/bn.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+

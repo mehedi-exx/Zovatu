dd7e20d9a037aef5938b2422362a9039769c07d5
Mehedi Hasan <158504182+mehedi-exx@users.noreply.github.com>
2025-07-24 01:03:22 +0600

    Create billing.js

---
+++ billing/billing.js
@@ -0,0 +1,5 @@
+document.getElementById("shopForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("✅ Shop Created! Billing System Activated.");
+  // এখানে পরে Shop DB তৈরি, Salesman ID, Permission ইত্যাদি আসবে।
+});
